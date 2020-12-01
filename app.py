68a0eb0c578c9e6140c4d7cdac0c9ffdf140a64f
andrezazz <67972321+andrezazz@users.noreply.github.com>
2020-11-30 23:42:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ import plotly.graph_objects as go
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
-import plotly.offline as pyo
 import dash
 from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-pyo.init_notebook_mode() ## ensures that the plotly graphics convert to HTML
+
 %%capture
 gss = pd.read_csv("https://github.com/jkropko/DS-6001/raw/master/localdata/gss2018.csv",
                  encoding='cp1252', na_values=['IAP','IAP,DK,NA,uncodeable', 'NOT SURE',
